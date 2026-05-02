73475112ea74b3dce2375aeb0a26e41810a892a4
AslanIsaev <bonscott.isaev@gmail.com>
2026-05-02 13:49:44 +0200

    built a custom MariaDB container based on Debian.

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Run temp server
+mysqls_safe --skip-networking &
+pid="$!"
+
+# Wait, while MariaDB starts
+echo "Waiting for MariaDB to start..."
+until mysqladmin ping -h "localhost" --silent; do
+	sleep 1
+done
+
+# Init
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+#Stops temp server
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+# Run MariaDB in foreground (PID 1 - right for Docker)
+exec mysgld_safe
